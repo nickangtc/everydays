f2862ca2994c723e9fc95e3a290a568b807e69fd
Nick Ang <nickangtc@gmail.com>
2022-12-25 14:11:17 +0100

    remove notes button from templates

--- templates/index.js
+++ templates/index.js
@@ -1,19 +1,5 @@
 addEventListener("DOMContentLoaded", start);
 
 function start() {
-  enableNotesToggle();
-  // ... other code
-}
-
-function enableNotesToggle() {
-  const collapseToggle = document.querySelector("#collapse-toggle");
-  const collapsible = document.querySelector(".collapsible");
-
-  collapseToggle.addEventListener("click", () => {
-    collapsible.classList.toggle("is-not-collapsed");
-  });
-  // If we're on localhost, show the notes by default
-  if (window.location.origin.includes("http://localhost")) {
-    collapsible.classList.toggle("is-not-collapsed");
-  }
+  // ... code
 }
