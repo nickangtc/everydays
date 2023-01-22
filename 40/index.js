aabb466eccfd5531910bdaedee1ea6e8a5dbd3f6
Nick Ang <nickangtc@gmail.com>
2023-01-22 14:20:36 +0100

    40 done

---
+++ 40/index.js
@@ -0,0 +1,14 @@
+addEventListener("DOMContentLoaded", start);
+
+function start() {
+  // ... code
+}
+
+function setup() {
+  createCanvas(320, 320);
+  frameRate(5);
+}
+
+function draw() {
+  // ... p5 code
+}
